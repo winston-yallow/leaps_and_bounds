05a904dc3282802498064da8f5845947d56a870c
Winston <44872771+winston-yallow@users.noreply.github.com>
2019-11-11 23:43:29 +0100

    Add base class for floating bodies

---
+++ game/floating_objects/FloatingBody.gd
@@ -0,0 +1,18 @@
+extends KinematicBody
+class_name FloatingBody
+
+var default_speed := 0.03
+var speed: float
+
+export var speed_modifier := 1.0
+
+func _ready() -> void:
+	# Allow the level scene to override the global speed of the scene.
+	# Uses default_speed as a fallback.
+	if 'speed' in get_tree().current_scene:
+		speed = get_tree().current_scene.speed
+	else:
+		speed = default_speed
+
+func _physics_process(delta: float) -> void:
+	move_and_slide(translation * speed * speed_modifier)
