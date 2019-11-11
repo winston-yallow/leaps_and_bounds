d5accd896a4138c58d52d60abab7652a218a2c2b
Winston <44872771+winston-yallow@users.noreply.github.com>
2019-11-12 00:43:21 +0100

    Add waiting and moving states to player

--- game/player/Player.gd
+++ game/player/Player.gd
@@ -1,8 +1,12 @@
 extends Spatial
 
+enum STATE { MOVING, WAITING }
+
 var mouse_sensitivity := 0.01
 var min_vertical_rotation := -75.0
 var max_vertical_rotation := +75.0
+
+var current_state: int = STATE.WAITING
 
 onready var cam_pivot := $CameraPivot
 
@@ -10,7 +14,7 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	if current_state == STATE.WAITING and event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		cam_pivot.rotate_x(-event.relative.y * mouse_sensitivity)
 		cam_pivot.rotation_degrees.x = clamp(
