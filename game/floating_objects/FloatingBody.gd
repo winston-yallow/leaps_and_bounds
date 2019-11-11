bc0c13f0bd380b7d2bfa35b988ec2ad8996714bb
Winston <44872771+winston-yallow@users.noreply.github.com>
2019-11-12 00:44:26 +0100

    Fix formatting: Use spaces instead of tabs

--- game/floating_objects/FloatingBody.gd
+++ game/floating_objects/FloatingBody.gd
@@ -5,12 +5,12 @@ var default_speed := 0.03
 var speed: float
 
 func _ready() -> void:
-	# Allow the level scene to override the global speed of the scene.
-	# Uses default_speed as a fallback.
-	if 'speed' in get_tree().current_scene:
-		speed = get_tree().current_scene.speed
-	else:
-		speed = default_speed
+    # Allow the level scene to override the global speed of the scene.
+    # Uses default_speed as a fallback.
+    if 'speed' in get_tree().current_scene:
+        speed = get_tree().current_scene.speed
+    else:
+        speed = default_speed
 
 func _physics_process(delta: float) -> void:
-	move_and_slide(translation * speed)
+    move_and_slide(translation * speed)
--- game/player/Player.gd
+++ game/player/Player.gd
@@ -11,14 +11,14 @@ var current_state: int = STATE.WAITING
 onready var cam_pivot := $CameraPivot
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event: InputEvent) -> void:
-	if current_state == STATE.WAITING and event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * mouse_sensitivity)
-		cam_pivot.rotate_x(-event.relative.y * mouse_sensitivity)
-		cam_pivot.rotation_degrees.x = clamp(
-			cam_pivot.rotation_degrees.x,
-			min_vertical_rotation,
-			max_vertical_rotation
-		)
+    if current_state == STATE.WAITING and event is InputEventMouseMotion:
+        rotate_y(-event.relative.x * mouse_sensitivity)
+        cam_pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+        cam_pivot.rotation_degrees.x = clamp(
+            cam_pivot.rotation_degrees.x,
+            min_vertical_rotation,
+            max_vertical_rotation
+        )
