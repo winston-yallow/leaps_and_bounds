2dcec681cb5a037cb3834990cf6bf6caba136f18
Winston <44872771+winston-yallow@users.noreply.github.com>
2019-11-17 00:42:38 +0100

    Show mouse cursor while in pause menu

--- game/menus/PauseMenu.gd
+++ game/menus/PauseMenu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var previous_mouse_mode
+
 func _ready() -> void:
     visible = false
 
@@ -7,3 +9,8 @@ func _input(event: InputEvent) -> void:
     if event.is_action_pressed("pause"):
         visible = !visible
         get_tree().paused = visible
+        if visible:
+            previous_mouse_mode = Input.get_mouse_mode()
+            Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+        else:
+            Input.set_mouse_mode(previous_mouse_mode)
